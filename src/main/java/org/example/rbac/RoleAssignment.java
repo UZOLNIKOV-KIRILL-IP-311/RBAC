70352689d2d18b890a437c6a418d29501ea97065
Узольников Кирилл Алексеевич <ip311s24@csc.sibsutis.ru>
2026-02-17 04:14:27 +0700

    add RoleAssignment interface

---
+++ src/main/java/org/example/rbac/RoleAssignment.java
@@ -0,0 +1,16 @@
+package org.example.rbac;
+
+public interface RoleAssignment {
+
+    String assignmentId();
+
+    User user();
+
+    Role role();
+
+    AssignmentMetadata metadata();
+
+    boolean isActive();
+
+    String assignmentType();
+}
